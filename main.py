bba0d44159856edfc026f733e2f209463d80a67b
live9807 <k76734182@gmail.com>
2025-01-05 19:04:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ start_time = time.time()
 mongo_client = pymongo.MongoClient(
     os.getenv(
         "MONGO_URI",
-        "Your mango db url"
+        "mongodb+srv://k76734182:Radhe@cluster0.qj64t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
 )
 db = mongo_client[os.getenv("MONGO_DB_NAME", "Rishu-free-db")]
